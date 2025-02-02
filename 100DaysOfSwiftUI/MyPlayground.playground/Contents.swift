4dbb81caef6b31d02184587cbf15f740e84175b3
jordanhwang <jordanwhwang@gmail.com>
2025-02-02 15:26:31 -0500

    Checkpoint 5: Closures
    https://www.hackingwithswift.com/quick-start/beginners/checkpoint-5

--- 100DaysOfSwiftUI/MyPlayground.playground/Contents.swift
+++ 100DaysOfSwiftUI/MyPlayground.playground/Contents.swift
@@ -1,24 +1,33 @@
-let team = ["Gloria", "Suzanne", "Piper", "Tiffany", "Tasha"]
+"""
+With closures under your belt, it’s time to try a little coding challenge using them.
 
+You’ve already met sorted(), filter(), map(), so I’d like you to put them together in a chain – call one, then the other, then the other back to back without using temporary variables.
 
-let captainFirstTeam = team.sorted { (name1, name2) -> Bool in
-    if name1 == "Suzanne" {
-        return true
-    } else if name2 == "Suzanne" {
-        return false
-    }
-    
-    return name1 < name2
-}
+Your input is this:
 
-let shorterClosure = team.sorted {
-    if $0 == "Suzanne" {
-        return true
-    } else if $1 == "Suzanne" {
-        return false
-    }
-    
-    return $0 < $1
-}
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+Your job is to:
 
-captainFirstTeam
+Filter out any numbers that are even
+Sort the array in ascending order
+Map them to strings in the format “7 is a lucky number”
+Print the resulting array, one item per line
+
+So, your output should be as follows:
+
+7 is a lucky number
+15 is a lucky number
+21 is a lucky number
+31 is a lucky number
+33 is a lucky number
+49 is a lucky number
+"""
+
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+
+luckyNumbers
+    .filter { !$0.isMultiple(of: 2) }
+    .sorted()
+    .map { "\($0) is a lucky number" }
+    .forEach { print($0) }
+
